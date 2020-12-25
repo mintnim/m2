5736cd55eb141d6524ffaf903c47627520d43771
lab302-15 <lab302-15@labeng.local>
2020-12-25 17:22:16 +0700

    complete

--- lab6_3.cpp
+++ lab6_3.cpp
@@ -1,19 +1,20 @@
-#include<iostream>
+#include <iostream>
+#include<string>
 using namespace std;
 
-char before(char x){
-	//Write your function definition here
-}
 
-int main(){
-	//Test Case
-	cout << before('A') << "\n";
-	cout << before('B') << "\n";
-	cout << before('P') << "\n";
-	cout << before('T') << "\n";
-	cout << before('Z') << "\n";
-	cout << before('a') << "\n";
-	cout << before('0') << "\n";
-	cout << before('c') << "\n";
-	return 0;
+char before(char x){
+	if(x == 'A'){
+		return 'Z';
+	
+	}
+	
+	
+	if(x >= 'A' and x <= 'Z' ){
+		return x - 1;
+	}
+	else{
+		return '0';
+	}
+	
 }
